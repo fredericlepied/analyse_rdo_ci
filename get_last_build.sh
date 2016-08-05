6d81927e465bea5481a0d6f012ad994d7511ee78
Frederic Lepied <frederic.lepied@redhat.com>
2016-08-05 20:49:54 +0200

    added get_last_build.sh

---
+++ get_last_build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 Red Hat, Inc.
+#
+# Author: Frederic Lepied <frederic.lepied@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+json=json.$$
+
+curl --fail -s -o $json https://ci.centos.org/view/rdo/view/promotion-pipeline/job/rdo-delorean-promote-master/lastBuild/api/json
+
+global_result=$(jq -r .result $json)
+
+if [ $global_result = FAILURE ]; then
+    echo "job $(jq -r .id $json):"
+    len=$(jq '.subBuilds | length' $json)
+
+    for idx in $(seq 0 $(($len - 1))); do
+        result=$(jq -r ".subBuilds[$idx].result" $json)
+        case $result in
+            FAILURE)
+                bn=$(jq -r ".subBuilds[$idx].buildNumber" $json)
+                jn=$(jq -r ".subBuilds[$idx].jobName" $json)
+                echo -n "    "
+                $(dirname $0)/analyse.sh $jn $bn
+                ;;
+        esac
+    done|sort
+else
+    echo "nothing to analyse ($global_result)" 1>&2
+fi
+
+rm $json
+
+# get_last_build.sh ends here
